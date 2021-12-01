5f1f3bf4ca44db3a901545543a73bf69e22ae144
Mikey O'Brien <mobrien@vectra.ai>
2021-12-01 11:24:59 -0600

    init commit

---
+++ 2021/day1.py
@@ -0,0 +1,20 @@
+def part1(input):
+    return sum([1 for i, line in enumerate(input[1:], start=1) if line > input[i-1]])
+
+def part2(input, step):
+    cnt = 0
+    windows = [input[i-step:i] for i in range(step-1, len(input))]
+    for i, window in enumerate(windows[1:], start=1):
+        print(sum(window), sum(windows[i-1]))
+        if sum(window) > sum(windows[i-1]):
+            cnt += 1
+    return cnt
+
+def main():
+    with open("day1.txt") as f:
+        lines = [int(line) for line in f.read().splitlines()]
+        print("part1", part1(lines))
+        print("part2", part2(lines, 3))
+
+if __name__ == "__main__":
+    main()
