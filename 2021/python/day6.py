bf074f2d7451fd4bd51ca88ae70a2f6ee30ce480
Mikey O'Brien <mobrien@vectra.ai>
2021-12-06 21:44:51 -0600

    working part1 day6

---
+++ 2021/python/day6.py
@@ -0,0 +1,23 @@
+TEST_INPUT = "3,4,3,1,2"
+PUZZLE_INPUT = "1,1,1,2,1,1,2,1,1,1,5,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,4,1,1,1,1,3,1,1,3,1,1,1,4,1,5,1,3,1,1,1,1,1,5,1,1,1,1,1,5,5,2,5,1,1,2,1,1,1,1,3,4,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,5,4,1,1,1,1,1,5,1,2,4,1,1,1,1,1,3,3,2,1,1,4,1,1,5,5,1,1,1,1,1,2,5,1,4,1,1,1,1,1,1,2,1,1,5,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,3,1,1,3,1,3,1,4,1,5,4,1,1,2,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,1,1,5,4,1,2,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,4,1,1,1,2,1,4,1,1,1,1,1,1,1,1,1,4,2,1,2,1,1,4,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,2,1,4,1,5,1,1,1,4,5,1,1,1,1,1,1,5,1,1,5,1,2,1,1,2,4,1,1,2,1,5,5,3"
+
+def simulate_lanternfish(input, days=80):
+    intervals = input
+    for i in range(days):
+        reset_indices = [i for i, timer in enumerate(intervals) if timer == 0]
+        intervals = [i-1 for i in intervals]
+        for k in reset_indices:
+            intervals[k] = 6
+        if reset_indices:
+            new_fish = [8] * len(reset_indices)
+            intervals += new_fish
+    return len(intervals)
+
+
+def main():
+    input = [int(i) for i in PUZZLE_INPUT.split(",")]
+    print(simulate_lanternfish(input, days=80))
+    print(simulate_lanternfish(input, days=256))  # part2
+
+if __name__ == '__main__':
+    main()
