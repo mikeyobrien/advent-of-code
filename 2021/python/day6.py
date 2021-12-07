5d7f7618cd0686d1366df57c8d39cf2b089d504f
Mikey O'Brien <mobrien@vectra.ai>
2021-12-06 22:07:29 -0600

    efficient part 2 solution

--- 2021/python/day6.py
+++ 2021/python/day6.py
@@ -1,17 +1,21 @@
 TEST_INPUT = "3,4,3,1,2"
 PUZZLE_INPUT = "1,1,1,2,1,1,2,1,1,1,5,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,4,1,1,1,1,3,1,1,3,1,1,1,4,1,5,1,3,1,1,1,1,1,5,1,1,1,1,1,5,5,2,5,1,1,2,1,1,1,1,3,4,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,5,4,1,1,1,1,1,5,1,2,4,1,1,1,1,1,3,3,2,1,1,4,1,1,5,5,1,1,1,1,1,2,5,1,4,1,1,1,1,1,1,2,1,1,5,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,3,1,1,3,1,3,1,4,1,5,4,1,1,2,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,1,1,5,4,1,2,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,4,1,1,1,2,1,4,1,1,1,1,1,1,1,1,1,4,2,1,2,1,1,4,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,2,1,4,1,5,1,1,1,4,5,1,1,1,1,1,1,5,1,1,5,1,2,1,1,2,4,1,1,2,1,5,5,3"
 
-def simulate_lanternfish(input, days=80):
-    intervals = input
+
+def simulate_lanternfish(input, days=256):
+    timer_state = [0] * 9
+    for time in input:
+        timer_state[time] += 1
+    print(timer_state)
     for i in range(days):
-        reset_indices = [i for i, timer in enumerate(intervals) if timer == 0]
-        intervals = [i-1 for i in intervals]
-        for k in reset_indices:
-            intervals[k] = 6
-        if reset_indices:
-            new_fish = [8] * len(reset_indices)
-            intervals += new_fish
-    return len(intervals)
+        new_state = [0] * 9
+        for i, time in enumerate(timer_state[1:], start=1):
+            to_add = timer_state[0]
+            new_state[i - 1] = time
+        new_state[6] += to_add
+        new_state[8] += to_add
+        timer_state = new_state
+    return sum(timer_state)
 
 
 def main():
@@ -19,5 +23,6 @@ def main():
     print(simulate_lanternfish(input, days=80))
     print(simulate_lanternfish(input, days=256))  # part2
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
